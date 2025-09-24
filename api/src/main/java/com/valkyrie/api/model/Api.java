87b9327d5d8bb657d8117e01f32d21d20d28b1e9
valkyrie <rajarshibiswas5678@gmail.com>
2025-09-24 17:30:48 +0530

    trying..

--- api/src/main/java/com/valkyrie/api/model/Api.java
+++ api/src/main/java/com/valkyrie/api/model/Api.java
@@ -34,16 +34,16 @@ public class Api {
             ).and(
                 delete("entity-delete", "http://entity:8082", "/entity/**")
             ).and(
-            //     get("book-get", "http://book:8084", "/book/**")
-            // ).and(
-            //     post("book-post", "http://book:8084", "/book/**")
-            // ).and(
-            //     delete("book-delete", "http://book:8084", "/book/**")
-            get("book-get", "http://localhost:8084", "/book/**")
+                get("book-get", "http://book:8084", "/book/**")
             ).and(
-                post("book-post", "http://localhost:8084", "/book/**")
+                post("book-post", "http://book:8084", "/book/**")
             ).and(
-                delete("book-delete", "http://localhost:8084", "/book/**")
+                delete("book-delete", "http://book:8084", "/book/**")
+            // get("book-get", "http://localhost:8084", "/book/**")
+            // ).and(
+            //     post("book-post", "http://localhost:8084", "/book/**")
+            // ).and(
+            //     delete("book-delete", "http://localhost:8084", "/book/**")
             ).and(
                 get("fine-get", "http://fine:8085", "/fine/**")
             ).and(
